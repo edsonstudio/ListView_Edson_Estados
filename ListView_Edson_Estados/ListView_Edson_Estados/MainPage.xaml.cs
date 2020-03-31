151c12616d2a6fa4d19d8f287e463102b40646c4
edsont8 <55901214+edsont8@users.noreply.github.com>
2020-03-31 02:22:14 -0300

    Exercicio Listview

---
+++ ListView_Edson_Estados/ListView_Edson_Estados/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ListView_Edson_Estados.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListView_Edson_Estados
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            lista.ItemsSource = new List<Estados>
+            {
+                new Estados {Nome = "São Paulo", Capital = "São Paulo", ImagemUrl = "sp.png"},
+                new Estados {Nome = "Pernambuco", Capital = "Recife", ImagemUrl = "pe.png"},
+                new Estados {Nome = "Rio Grande do Norte", Capital = "Natal", ImagemUrl = "rn.png"},
+                new Estados {Nome = "Minas Gerais", Capital = "Belo Horizonte", ImagemUrl = "mg.png"},
+                new Estados {Nome = "Bahia", Capital = "Salvador", ImagemUrl = "ba.png"}
+            };           
+        }
+
+        public void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var estado = e.SelectedItem as Estados;
+            DisplayAlert($"Estado: {estado.Nome}", $"Capital: {estado.Capital}", "Ok");
+        }
+        public void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var estado = e.Item as Estados;
+            DisplayAlert($"Estado: {estado.Nome}", $"Capital: {estado.Capital}", "Ok");
+        }
+
+        ListView HeaderList = new ListView()
+        {
+            Header = "Header",
+            Footer = "Footer"
+        };
+    }
+}
---
+++ ListView_Edson_Estados/ListView_Edson_Estados/Models/Estados.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListView_Edson_Estados.Models
+{
+    public class Estados
+    {
+        public string Nome { get; set; }
+        public string Capital { get; set; }
+        public string ImagemUrl { get; set; }
+    }
+}
